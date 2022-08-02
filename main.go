e53377000e9e33ca504cf96cd2cdd4176a5dee60
gitkill <49554515+wuyongmax@users.noreply.github.com>
2022-08-02 13:41:13 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,31 +1,24 @@
 package main
-
 import (
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"strings"
-	"log"
-//	"flag"
-//	"github.com/golang/glog"
+	"work/logger"
 )
-
 func main() {
-//	flag.Set("v",4)
-//	glog.V(2).Info("in main")
+	logger.Init()
+	logger.Lg.Info("begin the process")
 	http.HandleFunc("/health", healthCheck)
 	http.HandleFunc("/GetComputeInfo", GetComputeInfo)
-
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		log.Fatal("server error 500")
+		logger.Lg.Error("server error 500")
 	}
 }
-
 func healthCheck(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "200 ok")
-
 }
 
 func GetComputeInfo(w http.ResponseWriter, r *http.Request) {
@@ -33,16 +26,11 @@ func GetComputeInfo(w http.ResponseWriter, r *http.Request) {
 	version := os.Getenv("VERSION")
 	addr := r.RemoteAddr
 	ar := strings.Split(addr, ":")
-	r.Header.Get("")
 	ip := ar[0]
-	// go func(){}()
-	fmt.Println("")
 	port := ar[1]
-	fmt.Println(fmt.Sprintf("client ip is %s port %s", ip, port))
+	logger.Lg.Info(fmt.Sprintf("client ip is %s port %s", ip, port))
 	for k, v := range r.Header {
 		w.Header().Set(k, v[0])
-
 	}
 	w.Header().Set("version", version)
-
 }
